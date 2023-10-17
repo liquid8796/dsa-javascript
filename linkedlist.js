3d6e3c20c3574afb6a0b4a28e8b0b4864613f935
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-10-17 08:47:37 +0000

    2023-10-17 15:47:37 +07:00 Asia/Saigon

--- linkedlist.js
+++ linkedlist.js
@@ -38,7 +38,7 @@ class LinkedList {
             this.tail = null;
         } else {
             let temp = this.head;
-            let pre = null;
+            let pre = this.head;
 
             while (temp.next) {
                 pre = temp;
