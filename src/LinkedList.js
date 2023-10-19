537b5fdef3875828b89998f9d55ad65a08fd6b14
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-10-19 02:08:10 +0000

    2023-10-19 09:08:10 +07:00 Asia/Saigon

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -119,6 +119,13 @@ class LinkedList {
         if (index < 0 || index > this.length) {
             return false;
         }
+
+        const newNode = new Node(value);
+        const temp = this.get(index - 1);
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+        this.length++;
     }
 }
 
