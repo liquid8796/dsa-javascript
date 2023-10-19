ca1a022f0ecc83cd0c5af3c0c805e172857fc0c0
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-10-19 01:07:28 +0000

    2023-10-19 08:07:28 +07:00 Asia/Saigon

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -1,113 +1,125 @@
 class Node {
-	constructor(value) {
-		this.value = value;
-		this.next = null;
-	}
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
 }
 
 class LinkedList {
-	constructor(value) {
-		const newNode = new Node(value);
-		this.head = newNode;
-		this.tail = this.head;
-		this.length = 1;
-	}
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.tail = this.head;
+        this.length = 1;
+    }
 
-	push(value) {
-		const newNode = new Node(value);
+    push(value) {
+        const newNode = new Node(value);
 
-		if (!this.head) {
-			this.head = newNode;
-			this.tail = newNode;
-		} else {
-			this.tail.next = newNode;
-			this.tail = newNode;
-		}
-		this.length++;
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+        this.length++;
 
-		return this;
-	}
+        return this;
+    }
 
-	pop() {
-		if (!this.head) {
-			return undefined;
-		}
+    pop() {
+        if (!this.head) {
+            return undefined;
+        }
 
-		let temp = this.head;
-		let pre = this.head;
+        let temp = this.head;
+        let pre = this.head;
 
-		while (temp.next) {
-			pre = temp;
-			temp = temp.next;
-		}
+        while (temp.next) {
+            pre = temp;
+            temp = temp.next;
+        }
 
-		this.tail = pre;
-		this.tail.next = null;
-		this.length--;
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
 
-		if (this.length === 0) {
-			this.head = null;
-			this.tail = null;
-		}
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
 
-		return temp;
-	}
+        return temp;
+    }
 
-	unshift(value) {
-		const newNode = new Node(value);
+    unshift(value) {
+        const newNode = new Node(value);
 
-		if (!this.head) {
-			this.head = newNode;
-			this.tail = newNode;
-		} else {
-			newNode.next = this.head;
-			this.head = newNode;
-		}
-		this.length++;
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
 
-		return this;
-	}
+        return this;
+    }
 
-	shift() {
-		if (!this.head) {
-			return undefined;
-		}
+    shift() {
+        if (!this.head) {
+            return undefined;
+        }
 
-		let temp = this.head;
-		this.head = this.head.next;
-		temp.next = null;
-		this.length--;
+        let temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.length--;
 
-		if (this.length === 0) {
-			this.tail = null;
-		}
+        if (this.length === 0) {
+            this.tail = null;
+        }
 
-		return temp;
-	}
+        return temp;
+    }
 
-	get(index) {
-		if (index < 0 || index >= this.length) {
-			return undefined;
-		}
+    get(index) {
+        if (index < 0 || index >= this.length) {
+            return undefined;
+        }
 
-		let temp = this.head;
-		for (let i = 0; i < index; i++) {
-			temp = temp.next;
-		}
+        let temp = this.head;
+        for (let i = 0; i < index; i++) {
+            temp = temp.next;
+        }
 
-		return temp;
-	}
+        return temp;
+    }
 
-	set(index, value) {
-		let temp = this.get(index);
+    set(index, value) {
+        let temp = this.get(index);
 
-		if (temp) {
-			temp.value = value;
-			return true;
-		}
+        if (temp) {
+            temp.value = value;
+            return true;
+        }
 
-		return false;
-	}
+        return false;
+    }
+
+    insert(index, value) {
+        if (index === 0) {
+            return this.unshift(value);
+        }
+        if (index === this.length) {
+            return this.push(value);
+        }
+        if (index < 0 || index > this.length) {
+            return false;
+        }
+    }
 }
 
 let myLinkedList = new LinkedList(11);
