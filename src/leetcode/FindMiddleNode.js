bb6648c7a941fa86613499cdf6c556fe68a6ca95
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-10-22 16:32:05 +0000

    2023-10-22 23:32:05 +07:00 Asia/Saigon

--- src/leetcode/FindMiddleNode.js
+++ src/leetcode/FindMiddleNode.js
@@ -56,10 +56,12 @@ class LinkedList {
 	findMiddleNode() {
 		let slow = this.head;
 		let fast = this.head;
+
 		while (fast !== null && fast.next !== null) {
 			slow = slow.next;
 			fast = fast.next.next;
 		}
+
 		return slow;
 	}
 }
