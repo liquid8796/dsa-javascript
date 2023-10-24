5f51815f16b060771e0a5f122650c850dae84190
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-10-24 16:09:18 +0000

    2023-10-24 23:09:18 +07:00 Asia/Saigon

--- src/leetcode/FindKthFromEnd.js
+++ src/leetcode/FindKthFromEnd.js
@@ -1,64 +1,77 @@
 class Node {
-    constructor(value) {
-        this.value = value;
-        this.next = null;
-    }
+	constructor(value) {
+		this.value = value;
+		this.next = null;
+	}
 }
 
 class LinkedList {
-    constructor(value) {
-        const newNode = new Node(value);
-        this.head = newNode;
-        this.tail = this.head;
-    }
+	constructor(value) {
+		const newNode = new Node(value);
+		this.head = newNode;
+		this.tail = this.head;
+	}
 
-    printList() {
-        let temp = this.head;
-        while (temp !== null) {
-            console.log(temp.value);
-            temp = temp.next;
-        }
-    }
+	printList() {
+		let temp = this.head;
+		while (temp !== null) {
+			console.log(temp.value);
+			temp = temp.next;
+		}
+	}
 
-    getHead() {
-        if (this.head === null) {
-            console.log("Head: null");
-        } else {
-            console.log("Head: " + this.head.value);
-        }
-    }
+	getHead() {
+		if (this.head === null) {
+			console.log('Head: null');
+		} else {
+			console.log('Head: ' + this.head.value);
+		}
+	}
 
-    getTail() {
-        if (this.tail === null) {
-            console.log("Tail: null");
-        } else {
-            console.log("Tail: " + this.tail.value);
-        }
-    }
+	getTail() {
+		if (this.tail === null) {
+			console.log('Tail: null');
+		} else {
+			console.log('Tail: ' + this.tail.value);
+		}
+	}
 
-    makeEmpty() {
-        this.head = null;
-        this.tail = null;
-        this.length = 0;
-    }
+	makeEmpty() {
+		this.head = null;
+		this.tail = null;
+		this.length = 0;
+	}
 
-    push(value) {
-        const newNode = new Node(value);
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            this.tail.next = newNode;
-            this.tail = newNode;
-        }
-    }
+	push(value) {
+		const newNode = new Node(value);
+		if (!this.head) {
+			this.head = newNode;
+			this.tail = newNode;
+		} else {
+			this.tail.next = newNode;
+			this.tail = newNode;
+		}
+	}
 
-    // WRITE THE FINDKTFROMEND METHOD HERE //
-    //                                     //
-    //                                     //
-    //                                     //
-    //                                     //
-    /////////////////////////////////////////
+	findKthFromEnd(k) {
+		let slow = this.head;
+		let fast = this.head;
+
+		for (let i = 0; i < k; i++) {
+			if (fast === null) {
+				return null;
+			}
+
+			fast = fast.next;
+		}
+
+		while (fast !== null) {
+			slow = slow.next;
+			fast = fast.next;
+		}
+
+		return slow;
+	}
 }
 
 let myLinkedList = new LinkedList(1);
@@ -67,7 +80,7 @@ myLinkedList.push(3);
 myLinkedList.push(4);
 myLinkedList.push(5);
 
-console.log("Original list:");
+console.log('Original list:');
 myLinkedList.printList();
 
 const k = 2;
@@ -75,9 +88,9 @@ const kthNodeFromEnd = myLinkedList.findKthFromEnd(k);
 
 console.log(`\n${k}th node from the end:`);
 if (kthNodeFromEnd) {
-    console.log(kthNodeFromEnd.value);
+	console.log(kthNodeFromEnd.value);
 } else {
-    console.log("Not found");
+	console.log('Not found');
 }
 
 /*
