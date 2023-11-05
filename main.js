7af72fec1c236376f4bd2e7739652254ebc5f147
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-05 16:30:46 +0000

    2023-11-05 23:30:46 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -1,6 +1,3 @@
 arr = [1, 3, 5];
-sum = arr.reduce((previous, current) => {
-	console.log(`prev = ${previous}`);
-	return previous + current;
-}, 3);
-console.log(`sum = ${sum}`);
+max = arr.reduce((previous, current) => (previous < current ? current : previous));
+console.log(`max = ${max}`);
