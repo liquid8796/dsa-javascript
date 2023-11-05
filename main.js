2782020124e9ea89a43b9878bd397e7402b5fdfc
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-05 16:40:06 +0000

    2023-11-05 23:40:06 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
-const arr1 = [0, 1, 2, [3, 4]];
-const arr2 = [
-	[0, 1],
-	[2, [3, [4, 5]]],
-];
-
-const result = arr2.flat(3);
-console.log(result);
+const arrayLike = {
+	length: 3,
+	0: [1, 2],
+	// Array-like objects aren't flattened
+	1: { length: 2, 0: 3, 1: 4 },
+	2: 5,
+	3: 3, // ignored by flat() since length is 3
+};
+console.log(Array.prototype.flat.call(arrayLike));
+// [ 1, 2, { '0': 3, '1': 4, length: 2 }, 5 ]
