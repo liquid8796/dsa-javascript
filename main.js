d897a9f8660881fc27e563b8283abca9124308d3
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-05 16:38:11 +0000

    2023-11-05 23:38:11 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const arr1 = [0, 1, 2, [3, 4]];
-const arr2 = [0, 1, [2, [3, [4, 5]]]];
+const arr2 = [
+	[0, 1],
+	[2, [3, [4, 5]]],
+];
 
-const result = arr2.flat(9);
+const result = arr2.flat(2);
 console.log(result);
