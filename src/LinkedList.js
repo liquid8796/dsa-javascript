c88534182cab6958b9de2a4488d38810d706a184
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-06 17:00:43 +0000

    2023-11-07 00:00:43 +07:00 Asia/Saigon

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -173,7 +173,7 @@ class LinkedList {
 		this.tail = mid;
 		console.log('tail = ', this.tail);
 		let fast = mid.next;
-		console.log(`fast = ${fast}`);
+		console.log('fast = ', fast);
 		let slow = null;
 
 		for (let i = 0; i < this.length; i++) {
