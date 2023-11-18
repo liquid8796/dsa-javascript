0822c988f881685d5154a311dd5da2a702becd73
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-18 11:14:08 +0000

    2023-11-18 18:14:08 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const arr = [1, 3, 2];
-const result = arr.reduce((prev, current) => prev + current);
+const a = [1, 3, 2];
+const b = [1, 2];
+const result = a.reduce((prev, current) => prev + current);
 
 console.log(result);
