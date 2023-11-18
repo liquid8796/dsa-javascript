86d55f2059c65d2e9c6b481cd12e033ba43ee280
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-18 15:10:26 +0000

    2023-11-18 22:10:26 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
-const a = [1, 3, 2];
-const b = [1, 2];
-b.forEach((el) => {
-    a.indexOf(el) !== -1 && a.splice(a.indexOf(el), 1);
-});
+class Node {
+    value;
+    next;
+}
 
-console.log(a);
+console.log(Object.getOwnPropertyNames(new Node()));
