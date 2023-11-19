35b2717e766801217a1112fb0d869877e5a2aeae
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-19 03:28:28 +0000

    2023-11-19 10:28:28 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
-const eval = 2;
+const café = 1; // This constant is named "caf\u{e9}"
+const café = 2; // This constant is different: "cafe\u{301}"
 
-console.log("\u{1F600}");
+console.log(`${café} - ${café}`);
