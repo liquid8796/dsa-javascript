b3278d6f2a88987e8939906d0291f8081290ba5d
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-19 16:12:38 +0000

    2023-11-19 23:12:38 +07:00 Asia/Saigon

--- src/LinkedList.js
+++ src/LinkedList.js
@@ -150,20 +150,20 @@ class LinkedList {
         return temp;
     }
 
-    // reverse() {
-    // 	let temp = this.head;
-    // 	this.head = this.tail;
-    // 	this.tail = temp;
-    // 	let next = temp.next;
-    // 	let prev = null;
+    reverse() {
+        let temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+        let next = temp.next;
+        let prev = null;
 
-    // 	for (let i = 0; i < this.length; i++) {
-    // 		next = temp.next;
-    // 		temp.next = prev;
-    // 		prev = temp;
-    // 		temp = next;
-    // 	}
-    // }
+        for (let i = 0; i < this.length; i++) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+    }
 
     reverse() {
         let mid = this.head;
