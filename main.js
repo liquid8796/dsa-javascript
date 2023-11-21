f1fe58914534706fd6eacf48a0cf07d90ff91065
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-21 08:17:05 +0000

    2023-11-21 15:17:05 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -59,7 +59,21 @@ class LinkedList {
         this.length++;
     }
 
-    hasLoop() {}
+    hasLoop() {
+        let fast = this.head;
+        let slow = this.head;
+
+        while (fast.next) {
+            fast = fast.next.next;
+            slow = slow.next;
+
+            if (fast === slow) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
 
 let myLinkedList = new LinkedList(1);
