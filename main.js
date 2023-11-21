4e45f31ea0353bb1cf02727f88adb7aa5a8a04d9
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-21 01:12:05 +0000

    2023-11-21 08:12:05 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -10,12 +10,40 @@ class LinkedList {
         const newNode = new Node(value);
         this.head = newNode;
         this.tail = this.head;
-        this.length = 1;
+    }
+
+    printList() {
+        let temp = this.head;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getHead() {
+        if (this.head === null) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: " + this.head.value);
+        }
+    }
+
+    getTail() {
+        if (this.tail === null) {
+            console.log("Tail: null");
+        } else {
+            console.log("Tail: " + this.tail.value);
+        }
+    }
+
+    makeEmpty() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
 
     push(value) {
         const newNode = new Node(value);
-
         if (!this.head) {
             this.head = newNode;
             this.tail = newNode;
@@ -23,153 +51,53 @@ class LinkedList {
             this.tail.next = newNode;
             this.tail = newNode;
         }
-        this.length++;
-
-        return this;
     }
 
-    pop() {
-        if (!this.head) {
-            return undefined;
-        }
-
-        let temp = this.head;
-        let pre = this.head;
-
-        while (temp.next) {
-            pre = temp;
-            temp = temp.next;
-        }
-
-        this.tail = pre;
-        this.tail.next = null;
-        this.length--;
-
-        if (this.length === 0) {
-            this.head = null;
-            this.tail = null;
-        }
-
-        return temp;
-    }
-
-    unshift(value) {
-        const newNode = new Node(value);
-
-        if (!this.head) {
-            this.head = newNode;
-            this.tail = newNode;
-        } else {
-            newNode.next = this.head;
-            this.head = newNode;
-        }
-        this.length++;
-
-        return this;
-    }
-
-    shift() {
-        if (!this.head) {
-            return undefined;
-        }
-
-        let temp = this.head;
-        this.head = this.head.next;
-        temp.next = null;
-        this.length--;
-
-        if (this.length === 0) {
-            this.tail = null;
-        }
-
-        return temp;
-    }
-
-    get(index) {
-        if (index < 0 || index >= this.length) {
-            return undefined;
-        }
-
-        let temp = this.head;
-        for (let i = 0; i < index; i++) {
-            temp = temp.next;
-        }
-
-        return temp;
-    }
-
-    set(index, value) {
-        let temp = this.get(index);
-
-        if (temp) {
-            temp.value = value;
-            return true;
-        }
-
-        return false;
-    }
-
-    insert(index, value) {
-        if (index === 0) {
-            return this.unshift(value);
-        }
-        if (index === this.length) {
-            return this.push(value);
-        }
-        if (index < 0 || index > this.length) {
-            return false;
-        }
-
-        const newNode = new Node(value);
-        const temp = this.get(index - 1);
-
-        newNode.next = temp.next;
-        temp.next = newNode;
-        this.length++;
-
-        return true;
-    }
-
-    remove(index) {
-        if (index === 0) {
-            return this.shift();
-        }
-        if (index === this.length - 1) {
-            return this.pop();
-        }
-        if (index < 0 || index >= this.length) {
-            return undefined;
-        }
-
-        const before = this.get(index - 1);
-        const temp = before.next;
-        before.next = temp.next;
-        temp.next = null;
-        this.length--;
-
-        return temp;
-    }
-
-    reverse() {
-        let tmp = this.head;
-        this.head = this.tail;
-        this.tail = tmp;
-        let next = tmp.next;
-        let prev = null;
-
-        while (next !== null) {
-            next = tmp.next;
-            tmp.next = prev;
-            prev = tmp;
-            tmp = next;
-        }
-    }
+    findMiddleNode() {}
 }
 
-let myLinkedList = new LinkedList(11);
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
 myLinkedList.push(3);
-myLinkedList.push(23);
-myLinkedList.push(7);
-myLinkedList.reverse();
+myLinkedList.push(4);
+myLinkedList.push(5);
 
-console.log(myLinkedList);
+console.log("Original list:");
+myLinkedList.printList();
+
+const middleNode = myLinkedList.findMiddleNode();
+console.log(`\nMiddle node value: ${middleNode.value}`);
+
+// Create a new list with an even number of elements
+let myLinkedList2 = new LinkedList(1);
+myLinkedList2.push(2);
+myLinkedList2.push(3);
+myLinkedList2.push(4);
+myLinkedList2.push(5);
+myLinkedList2.push(6);
+
+console.log("\nOriginal list 2:");
+myLinkedList2.printList();
+
+const middleNode2 = myLinkedList2.findMiddleNode();
+console.log(`\nMiddle node value of list 2: ${middleNode2.value}`);
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Original list:
+    1
+    2
+    3
+    4
+    5
+    Middle node value: 3
+    Original list 2:
+    1
+    2
+    3
+    4
+    5
+    6
+    Middle node value of list 2: 4
+*/
