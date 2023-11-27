6528ca61f67c53261ae038bb3f58d9557e89522d
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-27 08:51:08 +0000

    2023-11-27 15:51:08 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -14,19 +14,10 @@ class LinkedList {
 
     printList() {
         let temp = this.head;
-        let output = "";
-        if (temp === null) {
-            console.log("empty");
-            return;
-        }
         while (temp !== null) {
-            output += String(temp.value);
+            console.log(temp.value);
             temp = temp.next;
-            if (temp !== null) {
-                output += " -> ";
-            }
         }
-        console.log(output);
     }
 
     getHead() {
@@ -60,97 +51,46 @@ class LinkedList {
         this.length++;
     }
 
-    partitionList(x) {
-        if (this.head === null) return;
-
-        const dummy1 = new Node(0);
-        const dummy2 = new Node(0);
-        let prev1 = dummy1;
-        let prev2 = dummy2;
-        let current = this.head;
-
-        while (current) {
-            if (current.value < x) {
-                prev1.next = current;
-                prev1 = current;
-            } else {
-                prev2.next = current;
-                prev2 = current;
-            }
-
-            current = current.next;
-        }
-
-        prev2.next = null;
-        prev1.next = dummy2.next;
-
-        this.head = dummy1.next;
-    }
+    // WRITE THE REMOVEDUPLICATES METHOD HERE //
+    //                                        //
+    //                                        //
+    //                                        //
+    //                                        //
+    ////////////////////////////////////////////
 }
 
-//  +=====================================================+
-//  |                                                     |
-//  |          THE TEST CODE BELOW WILL PRINT             |
-//  |              OUTPUT TO "USER LOGS"                  |
-//  |                                                     |
-//  |  Use the output to test and troubleshoot your code  |
-//  |                                                     |
-//  +=====================================================+
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+myLinkedList.push(5);
 
-// Helper function to create list from array
-function createListFromArray(arr) {
-    const ll = new LinkedList(arr[0]);
-    for (let i = 1; i < arr.length; i++) {
-        ll.push(arr[i]);
-    }
-    return ll;
-}
+console.log("Original list:");
+myLinkedList.printList();
 
-// Helper function to compare list with array
-function listMatchesArray(ll, arr) {
-    let temp = ll.head;
-    let i = 0;
-    while (temp !== null && i < arr.length) {
-        if (temp.value !== arr[i]) {
-            return false;
-        }
-        temp = temp.next;
-        i++;
-    }
-    return temp === null && i === arr.length;
-}
+myLinkedList.removeDuplicates();
 
-// Function to run a single test
-function runTest(testNum, description, ll, x, expectedArr) {
-    console.log("---------------------------------------");
-    console.log(`Test ${testNum}: ${description}`);
-    console.log("BEFORE: ");
-    ll.printList();
-    console.log("PARTITION: " + x);
-    ll.partitionList(x);
-    console.log("AFTER: ");
-    ll.printList();
-    console.log(listMatchesArray(ll, expectedArr) ? "PASS" : "FAIL");
-}
+console.log("\nList after removing duplicates:");
+myLinkedList.printList();
 
-// Test 1: Basic partition
-let ll1 = createListFromArray([1, 4, 3, 2, 5, 2]);
-runTest(1, "Basic partition", ll1, 3, [1, 2, 2, 4, 3, 5]);
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Original list:
+    1
+    2
+    3
+    3
+    4
+    5
+    5
+    List after removing duplicates:
+    1
+    2
+    3
+    4
+    5
 
-// Test 2: No elements to partition
-let ll2 = createListFromArray([4, 5, 6]);
-runTest(2, "No elements to partition", ll2, 3, [4, 5, 6]);
-
-// Test 3: All elements smaller
-let ll3 = createListFromArray([1, 2, 2]);
-runTest(3, "All elements smaller", ll3, 3, [1, 2, 2]);
-
-// Test 4: Single-element list
-let ll4 = createListFromArray([1]);
-runTest(4, "Single-element list", ll4, 3, [1]);
-
-// Test 5: All elements equal to partition
-let ll5 = createListFromArray([3, 3, 3]);
-runTest(5, "All elements equal to partition", ll5, 3, [3, 3, 3]);
-
-console.log("---------------------------------------");
+*/
