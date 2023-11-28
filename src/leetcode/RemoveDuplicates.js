b74b7ec568ac129ee8da9dae7e3f43f3244d65b7
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-28 02:26:22 +0000

    2023-11-28 09:26:22 +07:00 Asia/Saigon

---
+++ src/leetcode/RemoveDuplicates.js
@@ -0,0 +1,108 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.next = null;
+    }
+}
+
+class LinkedList {
+    constructor(value) {
+        const newNode = new Node(value);
+        this.head = newNode;
+        this.length = 1;
+    }
+
+    printList() {
+        let temp = this.head;
+        while (temp !== null) {
+            console.log(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    getHead() {
+        if (this.head === null) {
+            console.log("Head: null");
+        } else {
+            console.log("Head: " + this.head.value);
+        }
+    }
+
+    getLength() {
+        console.log("Length: " + this.length);
+    }
+
+    makeEmpty() {
+        this.head = null;
+        this.length = 0;
+    }
+
+    push(value) {
+        const newNode = new Node(value);
+        if (!this.head) {
+            this.head = newNode;
+        } else {
+            let current = this.head;
+            while (current.next !== null) {
+                current = current.next;
+            }
+            current.next = newNode;
+        }
+
+        this.length++;
+    }
+
+    removeDuplicates() {
+        const values = new Set();
+        let prev = null;
+        let current = this.head;
+
+        while (current) {
+            if (values.has(current.value)) {
+                prev.next = current.next;
+                this.length -= 1;
+            } else {
+                values.add(current.value);
+                prev = current;
+            }
+
+            current = current.next;
+        }
+    }
+}
+
+let myLinkedList = new LinkedList(1);
+myLinkedList.push(2);
+myLinkedList.push(3);
+myLinkedList.push(3);
+myLinkedList.push(4);
+myLinkedList.push(5);
+myLinkedList.push(5);
+
+console.log("Original list:");
+myLinkedList.printList();
+
+myLinkedList.removeDuplicates();
+
+console.log("\nList after removing duplicates:");
+myLinkedList.printList();
+
+/*
+    EXPECTED OUTPUT:
+    ----------------
+    Original list:
+    1
+    2
+    3
+    3
+    4
+    5
+    5
+    List after removing duplicates:
+    1
+    2
+    3
+    4
+    5
+
+*/
