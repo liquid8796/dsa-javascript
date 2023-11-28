81220480415e60289a7776fcabd6676aa113fa0f
GEO\tran.le.ha.nam <tran.le.ha.nam@geonet.co.jp>
2023-11-28 05:26:16 +0000

    2023-11-28 12:26:16 +07:00 Asia/Saigon

--- main.js
+++ main.js
@@ -9,100 +9,167 @@ class LinkedList {
     constructor(value) {
         const newNode = new Node(value);
         this.head = newNode;
+        this.tail = this.head;
         this.length = 1;
-    }
-
-    printList() {
-        let temp = this.head;
-        while (temp !== null) {
-            console.log(temp.value);
-            temp = temp.next;
-        }
-    }
-
-    getHead() {
-        if (this.head === null) {
-            console.log("Head: null");
-        } else {
-            console.log("Head: " + this.head.value);
-        }
-    }
-
-    getLength() {
-        console.log("Length: " + this.length);
-    }
-
-    makeEmpty() {
-        this.head = null;
-        this.length = 0;
     }
 
     push(value) {
         const newNode = new Node(value);
+
         if (!this.head) {
             this.head = newNode;
+            this.tail = newNode;
         } else {
-            let current = this.head;
-            while (current.next !== null) {
-                current = current.next;
-            }
-            current.next = newNode;
+            this.tail.next = newNode;
+            this.tail = newNode;
         }
-
         this.length++;
+
+        return this;
     }
 
-    removeDuplicates() {
-        const values = new Set();
+    pop() {
+        if (!this.head) {
+            return undefined;
+        }
+
+        let temp = this.head;
+        let pre = this.head;
+
+        while (temp.next) {
+            pre = temp;
+            temp = temp.next;
+        }
+
+        this.tail = pre;
+        this.tail.next = null;
+        this.length--;
+
+        if (this.length === 0) {
+            this.head = null;
+            this.tail = null;
+        }
+
+        return temp;
+    }
+
+    unshift(value) {
+        const newNode = new Node(value);
+
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } else {
+            newNode.next = this.head;
+            this.head = newNode;
+        }
+        this.length++;
+
+        return this;
+    }
+
+    shift() {
+        if (!this.head) {
+            return undefined;
+        }
+
+        let temp = this.head;
+        this.head = this.head.next;
+        temp.next = null;
+        this.length--;
+
+        if (this.length === 0) {
+            this.tail = null;
+        }
+
+        return temp;
+    }
+
+    get(index) {
+        if (index < 0 || index >= this.length) {
+            return undefined;
+        }
+
+        let temp = this.head;
+        for (let i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+
+        return temp;
+    }
+
+    set(index, value) {
+        let temp = this.get(index);
+
+        if (temp) {
+            temp.value = value;
+            return true;
+        }
+
+        return false;
+    }
+
+    insert(index, value) {
+        if (index === 0) {
+            return this.unshift(value);
+        }
+        if (index === this.length) {
+            return this.push(value);
+        }
+        if (index < 0 || index > this.length) {
+            return false;
+        }
+
+        const newNode = new Node(value);
+        const temp = this.get(index - 1);
+
+        newNode.next = temp.next;
+        temp.next = newNode;
+        this.length++;
+
+        return true;
+    }
+
+    remove(index) {
+        if (index === 0) {
+            return this.shift();
+        }
+        if (index === this.length - 1) {
+            return this.pop();
+        }
+        if (index < 0 || index >= this.length) {
+            return undefined;
+        }
+
+        const before = this.get(index - 1);
+        const temp = before.next;
+        before.next = temp.next;
+        temp.next = null;
+        this.length--;
+
+        return temp;
+    }
+
+    reverse() {
+        let temp = this.head;
+        this.head = this.tail;
+        this.tail = temp;
+        let next = temp.next;
         let prev = null;
-        let current = this.head;
 
-        while (current) {
-            if (values.has(current.value)) {
-                prev.next = current.next;
-                this.length -= 1;
-            } else {
-                values.add(current.value);
-                prev = current;
-            }
-
-            current = current.next;
+        for (let i = 0; i < this.length; i++) {
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
         }
     }
 }
 
-let myLinkedList = new LinkedList(1);
-myLinkedList.push(2);
+let myLinkedList = new LinkedList(11);
 myLinkedList.push(3);
-myLinkedList.push(3);
-myLinkedList.push(4);
-myLinkedList.push(5);
-myLinkedList.push(5);
+myLinkedList.push(23);
+myLinkedList.push(7);
+myLinkedList.reverse();
 
-console.log("Original list:");
-myLinkedList.printList();
-
-myLinkedList.removeDuplicates();
-
-console.log("\nList after removing duplicates:");
-myLinkedList.printList();
-
-/*
-    EXPECTED OUTPUT:
-    ----------------
-    Original list:
-    1
-    2
-    3
-    3
-    4
-    5
-    5
-    List after removing duplicates:
-    1
-    2
-    3
-    4
-    5
-
-*/
+console.log(myLinkedList);
